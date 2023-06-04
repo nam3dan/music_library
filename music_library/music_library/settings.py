0e1b401aa279152a0c88cbf297508f1f66f59a3a
nam3dan <namillennial@gmail.com>
2023-06-04 11:15:57 -0500

    Modified settings.py and created local_settings.py

--- music_library/music_library/settings.py
+++ music_library/music_library/settings.py
@@ -19,8 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jwhuuxcq18!-9@%q#bu))19k!sv=k&kr*6qop3fvd9^u(+dx!i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -68,17 +66,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'music_library.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
